16af6e7919e28da1b1aa6d50b20b300481357a2d
kennysun101 <kennysun101@gmail.com>
2017-12-05 17:24:24 +0800

    0.0.1

---
+++ B/B/Targets/Target_B.h
@@ -0,0 +1,15 @@
+//
+//  Target_B.h
+//  B
+//
+//  Created by kennysun on 2017/12/4.
+//  Copyright © 2017年 kennysun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Target_B : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+@end
